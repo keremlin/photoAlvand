a0abda2eea8822f9ed48f5ffd47b94f752c93488
arash yeganeh rad <arash.yegane@gmail.com>
2021-04-12 23:33:46 +0430

    after icons

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,98 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import './font.css'
-
-import Header from './header/header'
+import './font.css';
+import CarouselUI from './crouselUI/carouselUI';
+import CardUI from './cardUI/cardUI';
+import GalleryUI from './galleryUI/galleryUI';
+import Header from './header/header';
+import babataher from './img/babataher.jpg';
+import sofal from './img/sofal2.jpg';
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons';
+import CameraIcon from '@material-ui/icons/Camera';
+import CameraRollIcon from '@material-ui/icons/CameraRoll';
+import CameraEnhanceIcon from '@material-ui/icons/CameraEnhance';
+import EcoIcon from '@material-ui/icons/Eco';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 function App() {
   return (
     <div className="App">
-       <Header></Header>
-      <section>
-        <span>section 1</span>
-      </section>
-      <section>
-        <span>section 2</span>
-      </section>
+      <Header></Header>
+      <div className="container-fluid">
+        <section>
+          <div class="row">
+            <div class="col-sm-12 vh-55 col-xs-12 m-0 p-0"><CarouselUI></CarouselUI></div>
+          </div>
+        </section>
+
+        <section>
+        <div class="row bg-light pt-3 pb-3">
+          <div class="col-sm-12 col-xs-12 ">
+            <span>
+              
+              <span class="pl-5">
+                <EcoIcon style={{ fontSize: 60 }}></EcoIcon>کیفیت بالا
+              </span>
+              <span class="pl-5">
+               <CameraRollIcon style={{ fontSize: 60 }}></CameraRollIcon> قیمت مناسب
+              </span>
+              <span class="pl-5">
+               <CameraEnhanceIcon style={{ fontSize: 60 }}></CameraEnhanceIcon> عکس سفارشی
+              </span>
+              <span class="pl-5">
+               <AddShoppingCartIcon style={{ fontSize: 60,fill: "black" }}></AddShoppingCartIcon>  تحویل سریع
+              </span>
+              
+            </span>
+          </div>
+          <div class="col-sm-12 col-xs-12">
+            <h1 class="p-4">دسته بندی محتوی</h1>
+          </div>
+            <div class="col-sm-2  col-xs-12 m-0  col-centered">
+              <CardUI></CardUI>
+              </div>
+            <div class="col-sm-2  col-xs-12  col-centered">
+              <CardUI></CardUI>
+              </div>
+            <div class="col-sm-2  col-xs-12 mr-3 col-centered">
+              <CardUI></CardUI>
+              </div>
+            <div class="col-sm-2  col-xs-12 mr-3 col-centered">
+              <CardUI></CardUI>
+              </div>
+            <div class="col-sm-2  col-xs-12 mr-3 col-centered">
+              <CardUI></CardUI>
+              </div>
+          </div>
+        </section>
+
+        <section>
+          <div className="row mt-2 pl-2 pr-2">
+            <div className="col-sm-12 col-sm-12">
+              <GalleryUI></GalleryUI>
+            </div>
+          </div>
+
+        </section>
+
+      </div>
       <footer>
-        <span>Footer</span>
+        <div class="row footerUI pt-5">
+          <div class="col-sm-12 col-xs-12 minHeight200 bg-dark">
+            <div class="row">
+              <div class="col-sm-3">لینکهای مفید</div>
+              <div class="col-sm-3">درباره ما</div>
+              <div class="col-sm-3">شیکه های اجمتماعی</div>
+              <div class="col-sm-3"></div>
+            </div>
+            <div class="row">
+              <div class="col-sm-12 col-xs-12">
+                Copyright 2020 PhotoAlvand
+              </div>
+            </div>
+          </div>
+        </div>
       </footer>
     </div>
   );
---
+++ client/src/crouselUI/carouselUI.js
@@ -0,0 +1,53 @@
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import react from 'react';
+import styles from './crouselUI.module.css';
+import slide11 from './../img/11.jpg';
+import slide12 from './../img/12.jpg';
+import slide13 from './../img/13.jpg';
+import slide14 from './../img/babataher.jpg';
+import slide15 from './../img/sofal2.jpg';
+
+export default function CarouselUI() {
+    return (
+        <>
+            <Carousel>
+                <Carousel.Item >
+                    <img
+                        className="d-block vh-55 tales"
+                        src={slide14}
+                        alt="First slide"
+                    />
+                    <Carousel.Caption>
+                        <h3>بهترین های پرسپولیس</h3>
+                        <p>منتخبی از بهترین عکسهای بازی های تیم پرسپولیس</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block vh-55"
+                        src={slide15}
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>اماکن تاریخی</h3>
+                        <p>پوشش خبری بازی های تیم ملی </p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block vh-55"
+                        src={slide13}
+                        alt="Third slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h3>هواداران</h3>
+                        <p>عکسهای جنجالی از هوارداران ورزشهای مختلف</p>
+                    </Carousel.Caption>
+                </Carousel.Item>
+            </Carousel>
+        </>
+    );
+}
