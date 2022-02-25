dd6774966a33c771d0a5c7679806b4c73fcfaf59
arash yeganeh rad <arash.yegane@gmail.com>
2022-02-25 10:42:55 +0330

    loading module is updated with new animations

--- client/src/components/loading/loading.js
+++ client/src/components/loading/loading.js
@@ -1,13 +1,16 @@
 import React from "react";
 import CircularProgress from '@material-ui/core/CircularProgress';
 import styles from "./loading.module.css";
+import SquareLoader from "react-spinners/SquareLoader";
+import { css } from "@emotion/react";
 export default function Loading(props) {
     return (
         <>
             {(props.show ?
                 <div className={styles.loadingMinHeight}>
                     <div className={styles.LoadingCenterClass}>
-                        <CircularProgress color="secondary" className="styles.LoadingMarginAuto" /> 
+                        <SquareLoader css={override} color="rgba(108, 117, 125, 0.871)"/>
+                        <p className={styles.LoadingText}></p>
                         <p className={styles.LoadingText}>Please wait</p>
                     </div>
                 </div>
@@ -16,3 +19,9 @@ export default function Loading(props) {
         </>
     );
 }
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  color:green;
+`;
